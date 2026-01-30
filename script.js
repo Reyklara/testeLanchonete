e9b8dab2fcfccd30dccee54b306e6df5d08c844e
Reyklara <newprogrammer1984@gmail.com>
2026-01-29 21:30:17 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -12,3 +12,43 @@ function proxima(){
     }
     document.querySelector("#bola"+contador).checked = true
 }
+
+let sanduiche = document.querySelector("#lanche")
+let valor = 0
+
+
+sanduiche.addEventListener("change", comer)
+
+function comer(){
+    let escolha = sanduiche.value
+    if(escolha == "sanduiche1"){
+        valor = valor + 25
+    }
+        document.querySelector("#resposta").innerHTML = `O valor do seu pedido é ${valor}`
+
+    if(escolha == "coxinha"){
+        valor = valor + 7
+    }
+    document.querySelector("#resposta").innerHTML = `O valor do seu pedido é ${valor} cada.`
+
+    if(escolha == "pastel"){
+        valor = valor + 10
+    }
+    document.querySelector("#resposta").innerHTML = `O valor do seu pedido é ${valor} cada.`
+
+    if(escolha == "cigarrete"){
+        valor = valor + 15
+    }
+    document.querySelector("#resposta").innerHTML = `O valor do seu pedido é ${valor} cada.`
+
+    if(escolha == "cubano"){
+         valor = valor + 30
+    }
+    document.querySelector("#resposta").innerHTML = `O valor do seu pedido é ${valor}.`
+
+    if(escolha == "pizza"){
+        valor = valor + 60
+    }
+    document.querySelector("#resposta").innerHTML = `O valor do seu pedido é ${valor} qualquer sabor.`
+}
+
